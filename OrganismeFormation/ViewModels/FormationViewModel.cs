f4a682bee6efce660c540167b9b33314cf0ae3c7
MaxenceMax <maxence.montet01@gmail.com>
2017-03-06 15:58:25 +0100

    OK

---
+++ OrganismeFormation/ViewModels/FormationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrganismeFormation.ViewModels
+{
+    public class FormationViewModel
+    {
+        private Boolean LigueEstPorteuse { get; set; }
+
+    }
+}
