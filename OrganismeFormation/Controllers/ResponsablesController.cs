3c8c5319560562b334fc5ce1607949f3fa59ef1d
Alexandre Le Boucher <alex.lbj@wanadoo.fr>
2017-03-15 17:58:38 +0100

    Cahier de MAx
    a

---
+++ OrganismeFormation/Controllers/ResponsablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OrganismeFormation.Models;
+
+namespace OrganismeFormation.Controllers
+{
+    public class ResponsablesController : Controller
+    {
+        private GestionOFEntities db = new GestionOFEntities();
+
+        // GET: Responsables
+        public ActionResult Index()
+        {
+            return View(db.Responsable.ToList());
+        }
+
+        // GET: Responsables/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Responsable responsable = db.Responsable.Find(id);
+            if (responsable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(responsable);
+        }
+
+        // GET: Responsables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Responsables/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nom,Prenom,Licence,Telephone,Email,Password")] Responsable responsable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Responsable.Add(responsable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(responsable);
+        }
+
+        // GET: Responsables/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Responsable responsable = db.Responsable.Find(id);
+            if (responsable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(responsable);
+        }
+
+        // POST: Responsables/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nom,Prenom,Licence,Telephone,Email,Password")] Responsable responsable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(responsable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(responsable);
+        }
+
+        // GET: Responsables/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Responsable responsable = db.Responsable.Find(id);
+            if (responsable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(responsable);
+        }
+
+        // POST: Responsables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            Responsable responsable = db.Responsable.Find(id);
+            db.Responsable.Remove(responsable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
